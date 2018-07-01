7b8bd166869c457741490c9554d3cdaed309605d
Jovian Anderson <joviand.anderson@gmail.com>
2018-07-01 17:14:57 +0700

    white option destination

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -23,7 +23,7 @@
 
                 <div class="row">
 
-                    <div class="col-md-4 col-12">
+                    <div class="col-lg-4 col-12 mb-30">
                         <div class="list-wrapper">
 
                             {{--<div class="inline-link active">
@@ -60,7 +60,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-8 col-12">
+                    <div class="col-lg-8 col-12">
 
                         <h1 class="default-title">Destinations</h1>
 
